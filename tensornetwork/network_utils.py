35acdec6a6e4a8dfda523968ad7b1eb40209ae83
Martin <martin.ganahl@gmail.com>
2019-09-16 17:29:39 -0400

    removed network_utils.py for now

--- tensornetwork/network_utils.py
+++
@@ -1,4 +0,0 @@
-from tensornetwork.network_components import Edge, BaseNode
-from tensornetwork import network
-from tensornetwork import TensorNetwork
-from typing import Text, Sequence, Optional, List, Union
