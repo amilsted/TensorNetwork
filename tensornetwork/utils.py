51dfb20ec1be62ac6f0790416fc9f5b4d977f7d8
Martin <martin.ganahl@gmail.com>
2019-09-20 16:56:07 -0400

    added save and load routines for sets of nodes

--- tensornetwork/utils.py
+++ tensornetwork/utils.py
@@ -1,90 +1,110 @@
 import h5py
-from tensornetwork.network import TensorNetwork
+from typing import Collection, Union, BinaryIO
 from tensornetwork.component_factory import get_component
-from tensornetwork.network_components import Edge
+from tensornetwork.network_components import Edge, BaseNode
+import tensornetwork as tn
+import numpy as np
+string_type = h5py.special_dtype(vlen=str)
 
-# from tensornetwork.network_components import BaseNode
 
-# def connect(edge1: Edge, edge2: Edge) -> Edge:
-#   if edge1 is edge2:
-#     if isinstance(edge1.node1, FreeNode) and isinstance(edge1.node2, FreeNode):
-#       edge1, edge2 = edge1.break_edge()
-#       node1 = edge1.node1
-#       node2 = edge2.node1
-#     else:
-#       raise ValueError("Cannot connect edge '{}' to itself.".format(self))
-#   else:
-#     if isinstance(self.node1, FreeNode) and isinstance(other.node1, FreeNode):
-#       node1 = self.node1
-#       node2 = other.node1
-#       edge1 = self
-#       edge2 = other
-#     elif not (isinstance(self.node1, FreeNode) or
-#               isinstance(other.node1, FreeNode)):
-#       if self.dimension != other.dimension:
-#         raise ValueError("Cannot connect edges of unequal dimension. "
-#                          "Dimension of edge '{}': {}, "
-#                          "Dimension of edge '{}': {}.".format(
-#                              self, other.dimension, other, other.dimension))
+def save(nodes: Collection[BaseNode], path: Union[str, BinaryIO]):
+  """
+  Save a collection of nodes into hdf5 format.
+  Args:
+    nodes: A collection of connected nodes. All nodes have to connect within
+      `nodes`.
+    path: path to folder where network is saved.
+  """
+  if tn.reachable(nodes) > set(nodes):
+    raise ValueError(
+        "Some nodes in `nodes` are connected to nodes not contained in `nodes`. "
+        "Saving not possible.")
+  if len(set(nodes)) < len(nodes):
+    raise ValueError(
+        'Some nodes in `nodes` appear more than once. This is not supported')
+  #we need to iterate twice and order matters
+  edges = list(tn.get_all_edges(nodes))
+  nodes = list(nodes)
 
-#       for edge in [self, other]:
-#         if not edge.is_dangling():
-#           raise ValueError("Edge '{}' is not a dangling edge. "
-#                            "This edge points to nodes: '{}' and '{}'".format(
-#                                edge, edge.node1, edge.node2))
-#       return self.node1.network.connect(self, other)
-#     else:
-#       raise TypeError(
-#           "Only nodes with same types can be connected. Got nodes with "
-#           "different types type(node1) = {} and type(node2) = {}.".format(
-#               type(self.node1), type(other.node1)))
-#   axis1_num = node1.get_axis_number(edge1.axis1)
-#   axis2_num = node2.get_axis_number(edge2.axis1)
-#   new_edge = Edge(None, node1, axis1_num, node2, axis2_num)
-#   node1.add_edge(new_edge, axis1_num)
-#   node2.add_edge(new_edge, axis2_num)
-#   return new_edge
+  old_edge_names = {n: edge.name for n, edge in enumerate(edges)}
+  old_node_names = {n: node.name for n, node in enumerate(nodes)}
 
-# def get_shared_edges(node1: BaseNode, node2: BaseNode) -> Set[Edge]:
-#   """Get all edges shared between two nodes.
+  #unique names for nodes and edges
+  for n, node in enumerate(nodes):
+    node.set_name('node{}'.format(n))
 
-#   Args:
-#     node1: The first node.
-#     node2: The second node.
+  for e, edge in enumerate(edges):
+    edge.set_name('edge{}'.format(e))
 
-#   Returns:
-#     A (possibly empty) `set` of `Edge`s shared by the nodes.
-#   """
-#   nodes = {node1, node2}
-#   shared_edges = set()
-#   # Assuming the network is well formed, all of the edges shared by
-#   # these two nodes will be stored in just one of the nodes, so we only
-#   # have to do this loop once.
-#   for edge in node1.edges:
-#     if set(edge.get_nodes()) == nodes:
-#       shared_edges.add(edge)
-#   return shared_edges
+  with h5py.File(path, 'w') as net_file:
+    nodes_group = net_file.create_group('nodes')
+    node_names_group = net_file.create_group('node_names')
+    node_names_group.create_dataset(
+        'names',
+        dtype=string_type,
+        data=np.array(list(old_node_names.values()), dtype=object))
+
+    edges_group = net_file.create_group('edges')
+    edge_names_group = net_file.create_group('edge_names')
+    edge_names_group.create_dataset(
+        'names',
+        dtype=string_type,
+        data=np.array(list(old_edge_names.values()), dtype=object))
+
+    for n, node in enumerate(nodes):
+      node.set_name('node{}'.format(n))
+
+      node_group = nodes_group.create_group(node.name)
+      node._save_node(node_group)
+      for edge in node.edges:
+        if edge.node1 == node and edge in edges:
+          edge_group = edges_group.create_group(edge.name)
+          edge._save_edge(edge_group)
+          edges.remove(edge)
+
+  #name edges and nodes back  to their original names
+  for n, node in enumerate(nodes):
+    nodes[n].set_name(old_node_names[n])
+
+  for n, edge in enumerate(edges):
+    edges[n].set_name(old_edge_names[n])
 
 
 def load(path: str):
-  """Load a tensor network from disk.
+  """
+  Load a set of nodes from disk.
 
   Args:
     path: path to folder where network is saved.
   """
+  nodes_list = []
+  edges_list = []
   with h5py.File(path, 'r') as net_file:
-    net = TensorNetwork(backend=net_file["backend"][()])
     nodes = list(net_file["nodes"].keys())
-    edges = list(net_file["edges"].keys())
+    node_names = {
+        'node{}'.format(n): v
+        for n, v in enumerate(net_file["node_names"]['names'][()])
+    }
 
-    for node_name in nodes:
+    edge_names = {
+        'edge{}'.format(n): v
+        for n, v in enumerate(net_file["edge_names"]['names'][()])
+    }
+    edges = list(net_file["edges"].keys())
+    #print(net_file["nodes"]['node_names'][()])
+    #print(net_file["edges"]['edge_names'][()])
+    for n, node_name in enumerate(nodes):
       node_data = net_file["nodes/" + node_name]
       node_type = get_component(node_data['type'][()])
-      node_type._load_node(net, node_data)
-
-    nodes_dict = {node.name: node for node in net.nodes_set}
-
-    for edge in edges:
+      nodes_list.append(node_type._load_node(node_data))
+    nodes_dict = {node.name: node for node in nodes_list}
+    for n, edge in enumerate(edges):
       edge_data = net_file["edges/" + edge]
-      Edge._load_edge(edge_data, nodes_dict)
-  return net
+      edges_list.append(Edge._load_edge(edge_data, nodes_dict))
+
+  for edge in edges_list:
+    edge.set_name(edge_names[edge.name])
+  for node in nodes_list:
+    node.set_name(node_names[node.name])
+
+  return nodes_list
